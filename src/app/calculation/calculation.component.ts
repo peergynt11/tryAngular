f0f2cd4de0ee6c98b8b7f5d2dc498dcff8211390
Bernd Vierhuf <bernd.vierhuf@siemens.com>
2018-03-19 17:28:09 +0100

    calculation component

--- src/app/calculation/calculation.component.ts
+++ src/app/calculation/calculation.component.ts
@@ -21,10 +21,10 @@ export class CalculationComponent implements OnInit {
   storages: IDropdown[];
   backup  : IDropdown[];
   
-  price_per_year      : Observable<IValue[]>       
-	price_storage_per_gb: Observable<IValue[]>
-	price_backup_per_gb : Observable<IValue[]> 
-	price_core_per_hour : Observable<IValue[]> 
+  price_per_year      : IValue;
+	price_storage_per_gb: IValue;
+	price_backup_per_gb : IValue;
+	price_core_per_hour : IValue;
   
   errorMessage: string;
 
@@ -34,25 +34,24 @@ export class CalculationComponent implements OnInit {
   ngOnInit() {
 
     this.calculationForm = this._fb.group({
-      bundle:'',
-      storage:'',
-      user:'',
-      result:'',
-      backup:''
+      bundle:'10000',
+      storage:'250',
+      user:'1',
+      result:'153.400',
+      backup:'0'
     })
 
     this.initSelectBoxes();
 
-    //subscribe nicht immer notwendig !! geht auch mit async-pipe:
-    this.price_per_year = this._calculationService.getSingleValue(4);
-    this.price_storage_per_gb = this._calculationService.getSingleValue(5);
-    this.price_backup_per_gb = this._calculationService.getSingleValue(6);
-    this.price_core_per_hour = this._calculationService.getSingleValue(7);
+    this._calculationService.getSingleValue(4).subscribe(data =>  this.price_per_year = data[0])
+    this._calculationService.getSingleValue(5).subscribe(data =>  this.price_storage_per_gb = data[0])
+    this._calculationService.getSingleValue(6).subscribe(data =>  this.price_backup_per_gb = data[0])
+    this._calculationService.getSingleValue(7).subscribe(data =>  this.price_core_per_hour = data[0])        
 
     this.calculationForm.get('bundle').valueChanges.subscribe(value => this.calculate(value));
-    this.calculationForm.get('user').valueChanges.subscribe(value => this.calculate(value));
     this.calculationForm.get('storage').valueChanges.subscribe(value => this.calculate(value));
-    this.calculationForm.get('bundle').valueChanges.subscribe(value => this.calculate(value));
+    this.calculationForm.get('user').valueChanges.subscribe(value => this.calculate(value));
+    this.calculationForm.get('backup').valueChanges.subscribe(value => this.calculate(value));
     //    this.calculationForm.valueChanges.subscribe(value => this.calculate(value));
   }
 
@@ -83,14 +82,38 @@ export class CalculationComponent implements OnInit {
   }
 
   onSubmit(): void {
-
+    console.log(this.price_per_year.singleDesc)
+    console.log(this.price_storage_per_gb.singleDesc)
+    console.log(this.price_backup_per_gb.singleDesc)
+    console.log(this.price_core_per_hour.singleDesc)        
   }
 
   calculate(value) {
-    console.log('vvv')
-    let $calculate = Math.random() * 100;
-    this.calculationForm.patchValue({
-      result: $calculate
-    })
+
+    console.log(this.calculationForm)
+
+    let $bundle = this.calculationForm.get('bundle').value
+    let $user = this.calculationForm.get('user').value
+    let $storage = this.calculationForm.get('storage').value
+    let $backup = this.calculationForm.get('backup').value
+
+
+    console.log(this.price_per_year.singleValue)
+    console.log(this.price_storage_per_gb.singleValue/100)    
+    console.log(this.price_backup_per_gb.singleValue/100)    
+    console.log(this.price_core_per_hour.singleValue/100)    
+
+
+    let $costs = ($user * this.price_per_year.singleValue) + 
+    ($user*$storage*(this.price_storage_per_gb.singleValue/100)) + 
+    ($user*$backup*(this.price_backup_per_gb.singleValue/100))
+
+    let $corehours = ($bundle - $costs) / (this.price_core_per_hour.singleValue/100);
+
+    if ($corehours <= 0) {
+      this.calculationForm.patchValue({result: "Please select a larger bundle"})
+    }else {
+      this.calculationForm.patchValue({result: $corehours.toLocaleString()})      
+    }
   }
 }
